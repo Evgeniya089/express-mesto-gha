2847850a430bd8ef5cb08c3095e261b56c8acf7d
Evgeniya Platonova <117095372+Evgeniya089@users.noreply.github.com>
2023-12-29 19:24:42 +0300

    Add files via upload

---
+++ errors/errorCodes.js
@@ -0,0 +1,11 @@
+const ERR_CODES = {
+  сreated: 201,
+  badRequest: 400,
+  notFound: 404,
+  conflictCode: 409,
+  internalServerError: 500,
+  unauthorized: 401,
+  forbidden: 403,
+};
+
+module.exports = ERR_CODES;
