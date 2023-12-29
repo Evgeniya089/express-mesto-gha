2bd202a886f89f869c7b5302869dfbcdc327c954
Evgeniya Platonova <117095372+Evgeniya089@users.noreply.github.com>
2023-12-29 10:47:11 +0300

    Add files via upload

---
+++ errors/conflict.js
@@ -0,0 +1,9 @@
+const { conflictCode } = require('./errorCodes');
+
+class ConflictErr extends Error {
+  constructor(message) {
+    super(message);
+    this.statusCode = conflictCode;
+  }
+}
+module.exports = ConflictErr;
